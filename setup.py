0e88b86c96d26d7806334b1645e93c8d85d7900f
Alexander Pilz <pilz@syslab.com>
2013-05-30 07:14:30 +0200

    Preparing release 0.7

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.7.dev0'
+version = '0.7'
 shortdesc = 'Restfully access the vortex via http.'
 
 #longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
