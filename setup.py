86db35f2e12594b8db64ae71e8cf61ff758b1cef
Alexander Pilz <pilz@syslab.com>
2013-06-10 14:56:22 +0200

    Back to development: 0.10

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.9'
+version = '0.10.dev0'
 shortdesc = 'Restfully access the vortex via http.'
 
 #longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
